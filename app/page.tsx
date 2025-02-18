19e2c6f5977fe708ca6163a0c176e5c91c4d05f9
Ludovico Cammarata <60526281+CammarataLudovico@users.noreply.github.com>
2025-02-18 23:35:53 +0100

    V3.2 Added Favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,43 @@
+"use client"
+
+import { useEffect } from "react";
 import Hero from "./components/Hero"
 import About from "./components/About"
+import Education from "./components/Education"
 import Experience from "./components/Experience"
 import Skills from "./components/Skills"
 import Services from "./components/Services"
-import Education from "./components/Education"
 import Contact from "./components/Contact"
 import FloatingNav from "./components/floating-nav"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
+
+  useEffect(() => {
+    const link: HTMLLinkElement = document.createElement('link');
+    link.rel = 'icon';
+    link.href = '/favicon.ico';  // Favicon
+    document.head.appendChild(link);
+
+    // Cleanup (opzionale)
+    return () => {
+      document.head.removeChild(link);
+    };
+  }, []);
+
   return (
     <main className="bg-gray-50 dark:bg-gray-900 min-h-screen">
       <FloatingNav />
       <Hero />
-      <About /> 
+      <About />
       <Education />
       {/* <Experience />*/}
-      <Skills />
+       <Skills />
       {/* <Services /> */}
       <Contact />
       <SpeedInsights />
       <Analytics />
     </main>
-  )
+  );
 }
-
